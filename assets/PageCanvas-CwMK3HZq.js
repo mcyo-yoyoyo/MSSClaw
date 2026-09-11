223b3223deb747daa2aef232d30d1f4b8d03298c
jh248 <jh248@users.noreply.github.com>
2026-09-11 10:03:13 +0000

    deploy: 66ab9de0d5b684a8800aa77a57e82cf2a201e101

---
+++ assets/PageCanvas-CwMK3HZq.js
@@ -0,0 +1,1 @@
+import{j as n}from"./vendor-react-3lf4XbMO.js";import{ad as o}from"./artifact-CPnumqde.js";function m({children:a,className:s,as:r="div"}){return n.jsx(r,{className:o("page-canvas",s),children:a})}export{m as P};
---
+++ assets/aiNews-COsmpbdq.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-C3mklPYO.js";import"./page-task-CZiHq_iJ.js";import"./aiNewsStore-xEFDmus2.js";function a(){const e=t.getState(),s=e.messages.filter(n=>n.kind!=="ai_news");s.length!==e.messages.length&&(t.setState({messages:s}),e.persist())}export{a as e};
